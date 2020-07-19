9dae428362b3192a6c4c63f75ab4abc38f3cdf94
vladfedoriuk <vlad.fedoriuk2000@gmail.com>
2020-07-19 14:56:56 +0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,24 @@
+import requests
+
+def send(url, **data):
+	try:
+		response = requests.post(
+			url=url,
+			data=data,
+			)
+
+		print(response.text)
+		response.raise_for_status()
+	except requests.HTTPError as he:
+		print(f'http error: {he}')
+	except Exception as e:
+		print(f'Exception: {e}')
+
+if __name__=='__main__':
+	#send('http://127.0.0.1:5000', name='vlad', email='fed@ukr.net', job='IT', date=5)
+	#send('http://127.0.0.1:5000/locales', locale='fr')
+	# send('http://127.0.0.1:5000/form/user', email="vlad@ukr.net", password="123456", confirmation="12356")
+	print(requests.get(url='http://127.0.0.1:5000/').text)
+	for num in range(1, 11):
+		print(num)
+		send('http://127.0.0.1:5000/guess', guess = num)
